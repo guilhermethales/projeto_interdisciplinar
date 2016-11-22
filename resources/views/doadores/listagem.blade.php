9ccff597035e4657050b22fa4a211c3d6ae3806b
Guilherme Thales <guilhermevtog@hotmail.com>
2016-11-22 16:53:35 -0200

    modalExclusao

--- resources/views/doadores/listagem.blade.php
+++ resources/views/doadores/listagem.blade.php
@@ -60,11 +60,6 @@
                         </a>
                     </td>
                     <td width="20px">
-                        <a href="" title="Editar">
-                            <span class="glyphicon glyphicon-pencil"></span>
-                        </a>
-                    </td>
-                    <td width="20px">
                         <a data-toggle="modal" data-target="#delete-modal" href="#"
                            data-href="/doadores/remove/{{ $d->id }}">
                             <span class="glyphicon glyphicon-trash"></span>
@@ -80,21 +75,5 @@
     <!-- /.box-body -->
 
             <!-- Modal DELETE -->
-    <div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" data-backdrop="static">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
-                    <h4 class="modal-title" id="modalLabel">Excluir Doador</h4>
-                </div>
-                <div class="modal-body">
-                    Deseja realmente excluir este doador?
-                </div>
-                <div class="modal-footer">
-                    <a class="btn btn-primary btn-ok">Sim</a>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('layouts.modalExclusao')
 @stop
---
+++ resources/views/layouts/modalExclusao.blade.php
@@ -0,0 +1,17 @@
+<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" data-backdrop="static">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="modalLabel">Excluir Doador</h4>
+            </div>
+            <div class="modal-body">
+                Deseja realmente excluir este doador?
+            </div>
+            <div class="modal-footer">
+                <a class="btn btn-primary btn-ok">Sim</a>
+                <button type="button" class="btn btn-default" data-dismiss="modal">N&atilde;o</button>
+            </div>
+        </div>
+    </div>
+</div>
